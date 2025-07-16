264e9192b615f0348df774ec4ccba5ca60847bf3
Felipe Riquelme <f2013839@gmail.com>
2025-07-15 22:59:28 -0300

    Adição de páginas artigos, 1,2,3,4 e 5; campanhas.html, coloquei os artigos no href da barra leteral, vizualização media queries, e novas rotas no run

--- Ponto Cidadão Version 1.2/run.py
+++ Ponto Cidadão Version 1.2/run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/serviços')
+@app.route('/servicos') 
 def servicos():
     return render_template('servicos.html')
 
@@ -18,6 +18,25 @@ def denuncie():
 def campanhas():
     return render_template('campanhas.html')
 
+@app.route('/artigo-1')
+def artigo1():
+    return render_template('artigo-1.html')
+
+@app.route('/artigo-2')
+def artigo2():
+    return render_template('artigo-2.html')
+
+@app.route('/artigo-3')
+def artigo3():
+    return render_template('artigo-3.html')
+
+@app.route('/artigo-4')
+def artigo4():
+    return render_template('artigo-4.html')
+
+@app.route('/artigo-5')
+def artigo5():
+    return render_template('artigo-5.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
